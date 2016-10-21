b8ee469b470e685143809f0081b5e5000328ace2
toshu91 <toshu91@gmail.com>
2016-10-21 15:22:34 -0400

    eck in from computer

---
+++ src/Testing.java
@@ -0,0 +1,11 @@
+package com.tosha;
+
+public clasee Testing(){
+
+public static void main(String args[]){
+
+System.out.println("Heelo World");
+System.out.println("Addition is: "+add(10,40));
+
+}
+}
