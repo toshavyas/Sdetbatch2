22ba7db5477f379f014cdf37e59066a0aee68852
toshu91 <toshu91@gmail.com>
2016-10-21 16:43:34 -0400

    for poll

--- src/Testing.java
+++ src/Testing.java
@@ -2,7 +2,7 @@ package com.tosha;
 
 public class Testing{
 
-public static void main(String args[]){
+public static void Main(String args[]){
 
 System.out.println("Heelo World");
 System.out.println("Addition is: "+add(10,40));
