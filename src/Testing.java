a8700770afd0c30ffbdf74e11dec419e00126d39
toshu91 <toshu91@gmail.com>
2016-10-21 16:13:38 -0400

    fix

--- src/Testing.java
+++ src/Testing.java
@@ -1,11 +1,16 @@
 package com.tosha;
 
-public clasee Testing(){
+public class Testing{
 
 public static void main(String args[]){
 
 System.out.println("Heelo World");
 System.out.println("Addition is: "+add(10,40));
 
+}
+
+public static int add(int a, int b){
+	return a+b;
+	
 }
 }
